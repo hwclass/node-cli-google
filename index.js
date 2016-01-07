c39ec7f5b71ac0e2d21e1f31c4b1a5e673195498
hwclass <harderworking.class@gmail.com>
2016-01-07 17:15:22 +0200

    Listed the links and make it possible to select the links to open in a new window

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 #! /usr/bin/env node
 
 var request = require('request');
+var querystring = require('querystring');
 var cheerio = require('cheerio');
 var cliselect = require('list-selector-cli');
 var util = require('util');
-
-var itemSel = 'li.g';
-
-//var requestUrl = 'http://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8';
-
-var requestUrl = 'http://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8';
-
-var options = {
-  url: requestUrl,
-  resultsPerPage : 10,
-  tld : 'com',
-  lang : 'en',
-  method: 'GET'
-};
+var windowOpen = require('open');
 
 var userArgs = process.argv.slice(2);
 
+var requestUrl = 'http://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8';
 
+var searchObj = searchObj || {};
+    searchObj.tld = 'com';
+    searchObj.lang = 'en';
+    searchObj.requestOptions = {};
+
+requestUrl = util.format(requestUrl, searchObj.tld, searchObj.lang, querystring.escape(userArgs), 0, searchObj.resultsPerPage);
+
+var options = { uri: requestUrl, method: 'GET'},
+    itemList = [];
 
 request(options, function(err, resp, body) {
   var responseString;
-  console.log(111);
   if ((err === null) && resp.statusCode === 200) {
-    console.log(222);
     var $ = cheerio.load(body);
-    console.log(body);
-    /*
-    $(itemSel).each(function (i, elem) {
-      console.log(elem);
+    //console.log('cheerio search list\n' + $('body div.g .rc h3 a').val());
+    var list = $('body .g');
+    $(list).each(function (ind, item) {
+      if (ind !== list.length) {
+        if (typeof item.children[0] !== 'undefined' && typeof item.children[0].children[0].attribs.href !== 'undefined') {
+          //console.dir(item.children[0].children[0].attribs.href);
+          itemList.push(unescape(item.children['0'].children['0'].attribs.href.split('/url?q=')[1]));
+          //console.dir(item.children[0].children[0].children[0].next.data);
+          //windowOpen(unescape(item.children['0'].children['0'].attribs.href.split('/url?q=')[1]));
+        }
+      }
     });
-    */
-  } else if (!!err) {
+  } else {
     new Error('Error on response' + (resp ? ' (' + resp.statusCode + ')' : '') + ':' + err + ' : ' + body);
   }
+
+  var keywordChoices = new cliselect(itemList);
+
+  var promise = keywordChoices.prompt();
+
+  promise.done(function (selection) {
+    for (var selectionIndex = 0, len = selection.length; selectionIndex < len; selectionIndex++) {
+      windowOpen(selection[selectionIndex]);
+    }
+  });
+
 });
-
-
-/* List selection
-
-var keywordChoices = new cliselect(userArgs);
-
-var promise = keywordChoices.prompt();
-
-promise.done(function (selection) {
-  console.log(selection);
-});
-*/
