ec8f4969b5ee2ce48251bab3a2dc18a6898cd9d3
hwclass <harderworking.class@gmail.com>
2016-01-07 14:00:28 +0200

    Tested some code

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ var cheerio = require('cheerio');
 var cliselect = require('list-selector-cli');
 var util = require('util');
 
+var itemSel = 'li.g';
+
 //var requestUrl = 'http://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8';
 
-var requestUrl = 'http://www.google.com/search';
+var requestUrl = 'http://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8';
 
 var options = {
   url: requestUrl,
+  resultsPerPage : 10,
+  tld : 'com',
+  lang : 'en',
   method: 'GET'
 };
 
@@ -20,10 +25,17 @@ var userArgs = process.argv.slice(2);
 
 request(options, function(err, resp, body) {
   var responseString;
+  console.log(111);
   if ((err === null) && resp.statusCode === 200) {
+    console.log(222);
     var $ = cheerio.load(body);
-    console.log(resp);
-  } else {
+    console.log(body);
+    /*
+    $(itemSel).each(function (i, elem) {
+      console.log(elem);
+    });
+    */
+  } else if (!!err) {
     new Error('Error on response' + (resp ? ' (' + resp.statusCode + ')' : '') + ':' + err + ' : ' + body);
   }
 });
